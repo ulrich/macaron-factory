08a9d22022ce395ab53d7ab15f0e4dfc3183a670
Ulrich VACHON <u.vachon@gmail.com>
2010-06-01 11:59:48 +0200

    Improved functionnal test for main page

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -6,7 +6,9 @@ import play.mvc.Http.Response;
 import play.test.FunctionalTest;
 
 public class ApplicationTest extends FunctionalTest {
-   private static final String QUOTE = "Imaginez une farandole de macarons";
+   private static final String INTRODUCTION_QUOTE = "Imaginez une farandole de macarons";
+   private static final String EVENT_DIV = "id=\"event\"";
+   private static final String COMPOSITION_DIV = "id=\"composition\"";
 
    @Test
    public void testThatIndexPageWorks() {
@@ -15,9 +17,16 @@ public class ApplicationTest extends FunctionalTest {
       assertContentType("text/html", response);
       assertCharset("utf-8", response);
 
+      // building index page string
       ByteArrayOutputStream baos = response.out;
       String indexPage = new String(baos.toByteArray());
       assertNotNull(indexPage);
-      assertTrue(indexPage.indexOf(QUOTE) > 1);
+      // finding the introduction quote
+
+      assertTrue(indexPage.indexOf(INTRODUCTION_QUOTE) > 1);
+      // finding events div
+      assertTrue(indexPage.indexOf(EVENT_DIV) > 1);
+      // finding compositions div
+      assertTrue(indexPage.indexOf(COMPOSITION_DIV) > 1);
    }
 }
