3700f34e29411e4aa43cb364d791b368c9fa4a18
Ulrich VACHON <u.vachon@gmail.com>
2010-06-03 00:18:01 +0200

    Used full templating model for all views

--- app/controllers/Compositions.java
+++ app/controllers/Compositions.java
@@ -7,11 +7,12 @@ import models.Composition;
 public class Compositions extends Application {
 
    public static void index() {
+      Long id = -1l;
       List<Long> compositionIds = Composition.find("select id from Composition").fetch(1, 1);
       if ((compositionIds != null) && !(compositionIds.isEmpty())) {
-         show(compositionIds.get(0));
+         id = compositionIds.get(0);
       }
-      render();
+      show(id);
    }
 
    public static void show(Long id) {
