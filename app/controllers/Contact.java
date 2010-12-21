9c834cdbda9d8af232f3c6e112523724af274400
Ulrich VACHON <u.vachon@gmail.com>
2010-12-21 02:19:54 +0100

    Used the new Play! framework version, fixed new API used for email

--- app/controllers/Contact.java
+++ app/controllers/Contact.java
@@ -5,6 +5,9 @@ import java.util.Date;
 
 import org.antlr.stringtemplate.StringTemplate;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
 
 import play.Logger;
 import play.Play;
@@ -30,20 +33,36 @@ public class Contact extends Controller {
    }
 
    private static void sendMail(models.Contact contact) {
-      String emailTemplate = null;
+      String forgedEmail = forgeEmail(contact);
+
+      Email email = new SimpleEmail();
       try {
-         emailTemplate = FileUtils.readFileToString(Play.getFile(EMAIL_TEMPLATE_FILENAME));
-      } catch (IOException e) {
-         Logger.warn("Unable to load email template file", e);
+         email.addTo("contact@macaron-factory.com");
+         email.setSubject("Contact par formulaire");
+         email.setFrom("website@macaron-factory.com");
+         email.setMsg(forgedEmail);
+      } catch (EmailException e) {
+         Logger.error("Unable to forge email", e);
          return;
       }
-      StringTemplate template = new StringTemplate(emailTemplate);
+      Mail.send(email);
+   }
+
+   private static String forgeEmail(models.Contact contact) {
+      StringTemplate template = null;
+
+      try {
+         template = new StringTemplate(FileUtils.readFileToString(Play.getFile(EMAIL_TEMPLATE_FILENAME)));
+      } catch (IOException e) {
+         Logger.error("Unable to load email template file", e);
+         return null;
+      }
       template.setAttribute("date", new Date());
       template.setAttribute("from", contact.email);
       template.setAttribute("fullname", contact.fullname);
       template.setAttribute("shortname", contact.shortname);
       template.setAttribute("message", contact.message);
 
-      Mail.send("website@macaron-factory.com", "contact@macaron-factory.com", "Contact par formulaire", template.toString());
+      return template.toString();
    }
 }
